00b8bf95842ca480139df413d6805cda3bc071fc
Ravikiran lv <ravikiranlv16@gmail.com>
2023-01-05 18:39:36 +0530

    added to git hub8

--- read_data_script/src/xml_to_excel_data_provider_excel/cross_browser_launching.java
+++ read_data_script/src/xml_to_excel_data_provider_excel/cross_browser_launching.java
@@ -26,7 +26,7 @@ public class cross_browser_launching {
 	public void Browser_Setup(@Optional("chrome") String browserName) throws IOException {
 		System.out.println(browserName);
 		if (browserName.equalsIgnoreCase("Chrome")) {
-			System.setProperty("webdriver.chrome.driver", "./softwares/chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "./softwares/chromedriver1.exe");
 			driver = new ChromeDriver();
 		} else if (browserName.equalsIgnoreCase("Firefox")) {
 			System.setProperty("webdriver.gecko.driver", "./softwares/geckodriver.exe");
