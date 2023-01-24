8cd485d46b0fedcdbda50e9962644306737545e2
Ravikiran lv <ravikiranlv16@gmail.com>
2023-01-24 10:39:04 +0530

    added

--- selenium/src/ELF_JAVA/REVERSE_THE_GIVEN_STRING.java
+++ selenium/src/ELF_JAVA/REVERSE_THE_GIVEN_STRING.java
@@ -6,25 +6,24 @@ import java.util.Scanner;
 
 public class REVERSE_THE_GIVEN_STRING {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the String value :");
 		String str = sc.nextLine();
-		
+
 		String[] strArray = str.split(" ");
-		int t = strArray.length;
-		for (String temp : strArray) {
-			System.out.println(temp);
-		}
-		
-		for (int i = 0; i <t; i++) {
+		// int t = strArray.length;
+		// for (String temp : strArray) {
+		// System.out.println(temp);
+		// }
+
+		for (int i = 0; i < strArray.length; i++) {
 			char[] s1 = strArray[i].toCharArray();
-			for (int j = s1.length-1; j >= 0; j--) {
-		          System.out.print(s1[j]);
+			for (int j = s1.length - 1; j >= 0; j--) {
+				System.out.print(s1[j]);
 			}
 			System.out.print(" ");
 		}
 	}
-
 }
 
 // O/P 
